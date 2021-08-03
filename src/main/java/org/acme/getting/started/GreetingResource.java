1791e5b4abcdeda75fd7f5be386f970b2b28c2a6
nilshartmann96 <hartmann_nils@outlook.de>
2021-08-03 13:13:08 +0200

    ELAB-456 localhost:8080/api/hello returns JSON Object with message "Hello World"

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -4,13 +4,17 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Collections;
+import java.util.Map;
 
 @Path("/api/hello")
 public class GreetingResource {
 
+    private final Map<String, String> message = Collections.singletonMap("message", "Hello World");
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello World!";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, String> sendMessage() {
+        return message;
     }
 }
