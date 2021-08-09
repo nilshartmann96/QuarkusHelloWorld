b86088d14fbf47f3b40a3332f5d43651332bc639
nilshartmann96 <hartmann_nils@outlook.de>
2021-08-09 09:36:12 +0200

    ELAB-457 Service added that returns a JSON Object with message "Hello World"

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -6,20 +6,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Collections;
-import java.util.Map;
 
 @Path("/api/hello")
 public class GreetingResource {
 
-    private final Map<String, String> message = Collections.singletonMap("message", "Hello World");
-
     @Inject
-    GreetingService service;
+    private GreetingService service;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response hello() {
-        return service.helloJSON(message);
+    public Response greet() {
+        return service.getGreeting();
     }
+
 }
--- src/main/java/org/acme/getting/started/GreetingService.java
+++ src/main/java/org/acme/getting/started/GreetingService.java
@@ -2,13 +2,13 @@ package org.acme.getting.started;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.core.Response;
-import java.util.Map;
+import java.util.Collections;
 
 @ApplicationScoped
 public class GreetingService {
 
-    public Response helloJSON(Map<String, String> message) {
-        return Response.ok(message).build();
+    public Response getGreeting() {
+        return Response.ok(Collections.singletonMap("message", "Hello World!")).build();
     }
 
 }
