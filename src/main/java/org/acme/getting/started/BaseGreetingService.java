149d5d7c9cc859dd9cca843d301e422148cfea92
nilshartmann96 <hartmann_nils@outlook.de>
2021-08-09 12:21:12 +0200

    ELAB-459 RepoGreetingService kann mit @Informal bei der Interface-Injection aufgerufen werden. BaseGreetingService wird aufgerufen, wenn keine Annotation an die Interface-Injection geschrieben wird.

--- src/main/java/org/acme/getting/started/BaseGreetingService.java
+++ src/main/java/org/acme/getting/started/BaseGreetingService.java
@@ -1,14 +1,15 @@
 package org.acme.getting.started;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Default;
 import java.util.Collections;
 import java.util.Map;
 
 @ApplicationScoped
-@Informal
-public class RepoGreetingService implements GreetingService {
+@Default
+public class BaseGreetingService implements GreetingService {
 
-    private final Map<String, String> message = Collections.singletonMap("message", "Hello World mit Qualifier-Implementierung!");
+    private final Map<String, String> message = Collections.singletonMap("message", "Hello World ohne Qualifier-Implementierung!");
 
     @Override
     public Map<String, String> getGreeting() {
